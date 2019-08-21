c256fa57d8ee987769c1add63e569110ad9351f6
lrh <lirh@hyxt.com>
2019-08-21 20:02:59 +0800

    spring web 中的restTemplate相关

--- rest-remplate-demo/src/main/java/com/lrh/restremplatedemo/RestRemplateDemoApplication.java
+++ rest-remplate-demo/src/main/java/com/lrh/restremplatedemo/RestRemplateDemoApplication.java
@@ -2,7 +2,11 @@ package com.lrh.restremplatedemo;
 
 import com.lrh.restremplatedemo.model.House;
 import com.lrh.restremplatedemo.model.MyTest;
+import com.lrh.restremplatedemo.support.CustomerConnectionKeepAliveStrategy;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,13 +15,17 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.annotation.Resource;
 import java.net.URI;
+import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @Slf4j
@@ -32,8 +40,12 @@ public class RestRemplateDemoApplication implements ApplicationRunner {
 //		SpringApplication.run(RestRemplateDemoApplication.class, args);
 	}
 
+
+
+
+
 	/**
-	 *
+	 *  默认用SimpleClientHttpRequestFactory 生成 restTemplate
 	 * @param restTemplateBuilder
 	 * @return
 	 */
@@ -45,12 +57,57 @@ public class RestRemplateDemoApplication implements ApplicationRunner {
 	@Autowired
 	private RestTemplate restTemplate;
 
+
+	/**
+	 * 采用 HttpComponentsClientHttpRequestFactory
+	 *
+	 * 还可以采用 OkHttp3ClientHttpRequestFactory
+	 *
+	 * Netty4ClientHttpRequestFactory  已经过时
+	 *
+	 * @return
+	 */
+	@Bean
+	public HttpComponentsClientHttpRequestFactory httpRequestFactory(){
+		PoolingHttpClientConnectionManager connectionManager =
+				new PoolingHttpClientConnectionManager(30, TimeUnit.SECONDS);
+		connectionManager.setMaxTotal(200);
+		connectionManager.setDefaultMaxPerRoute(20);
+
+		CloseableHttpClient httpClient = HttpClients.custom()
+				.setConnectionManager(connectionManager)
+				.evictIdleConnections(30, TimeUnit.SECONDS)
+				.disableAutomaticRetries()
+				// 有 Keep-Alive 认里面的值，没有的话永久有效
+				//.setKeepAliveStrategy(DefaultConnectionKeepAliveStrategy.INSTANCE)
+				// 换成自定义的
+				.setKeepAliveStrategy(new CustomerConnectionKeepAliveStrategy())
+				.build();
+
+		HttpComponentsClientHttpRequestFactory requestFactory =
+				new HttpComponentsClientHttpRequestFactory(httpClient);
+		return requestFactory;
+	}
+
+	@Bean("customerRestTemplate")
+	public RestTemplate restTemplateOfCustomerFactory(RestTemplateBuilder builder){
+		return builder
+				.setConnectTimeout(Duration.ofMillis(100))
+				.setReadTimeout(Duration.ofMillis(500))
+				.requestFactory(this::httpRequestFactory)
+				.build();
+	}
+
+
+	@Resource
+	private RestTemplate customerRestTemplate;
+
+
+
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 //		simpleRestTemplate();
-
 		parameterizedType();
-
 	}
 
 
@@ -74,7 +131,7 @@ public class RestRemplateDemoApplication implements ApplicationRunner {
 	}
 
 	/**
-	 * 接收泛型对象
+	 * 解析泛型对象
 	 */
 	private void parameterizedType(){
 		URI uri = UriComponentsBuilder.fromUriString("http://localhost:8080/all").build("");
@@ -85,7 +142,7 @@ public class RestRemplateDemoApplication implements ApplicationRunner {
 		//接受泛型对象 直接使用泛型对象会报错！！！
 		ParameterizedTypeReference<List<House>> pr = new ParameterizedTypeReference<List<House>>() {};
 
-		ResponseEntity<List<House>> exchange = restTemplate.exchange(requestEntity, pr);
+		ResponseEntity<List<House>> exchange = customerRestTemplate.exchange(requestEntity, pr);
 		log.info("exchange responseStatus {} ; response Headers {}", exchange.getStatusCode(), exchange.getHeaders());
 		log.info("exchange responseBody {}",exchange.getBody());
 	}
---
+++ rest-remplate-demo/src/main/java/com/lrh/restremplatedemo/support/CustomerConnectionKeepAliveStrategy.java
@@ -0,0 +1,29 @@
+package com.lrh.restremplatedemo.support;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.conn.ConnectionKeepAliveStrategy;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.protocol.HttpContext;
+
+import java.util.Arrays;
+
+/**
+ * 自定义的连接的keepAlive策略
+ */
+public class CustomerConnectionKeepAliveStrategy implements ConnectionKeepAliveStrategy {
+
+    private final long DEFAULT_SECONDS = 30;
+
+    @Override
+    public long getKeepAliveDuration(HttpResponse response, HttpContext context) {
+        return Arrays.asList(response.getHeaders(HTTP.CONN_KEEP_ALIVE))
+                .stream()
+                .filter(h -> StringUtils.equalsIgnoreCase(h.getName(), "timeout")
+                        && StringUtils.isNumeric(h.getValue()))
+                .findFirst()
+                .map(h -> NumberUtils.toLong(h.getValue(), DEFAULT_SECONDS))
+                .orElse(DEFAULT_SECONDS) * 1000;
+    }
+}
